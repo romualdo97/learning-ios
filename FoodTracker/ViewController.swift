ca23c23eda65bea0a781aa30461b042753112fdd
Manuela Recio <manuela.recio@wizard.com.co>
2020-06-03 15:57:02 -0500

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Wizard on 6/3/20.
+//  Copyright © 2020 Wizard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
